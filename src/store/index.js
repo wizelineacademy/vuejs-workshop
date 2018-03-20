51b177c35b8cddaf7898e44153ffa7e53a854447
Eder Diaz <ederchrono@gmail.com>
2018-03-20 13:32:56 -0600

    🚧 WIP store connected to service

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,29 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import movieService  from '../services/MovieService'
 
 Vue.use(Vuex)
 
 const store = {
   state: {
-    hello: 'world'
+    hello: 'world',
+    movies: []
   },
 
   actions: {
-
+    async fetchMovies (context) {
+      const response = await MovieService.getMovies({
+        page: 1,
+        genre: null
+      })
+      context.commit('setMovies', response.data)
+    }
   },
 
   mutations: {
-
+    setMovies(state, moviesData) {
+      state.movies = moviesData.results
+    }
   },
 
   getters: {
